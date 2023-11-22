044dad5ad0bd9460fd1810a1400304430031f3ec
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2023-11-21 22:59:51 -0300

    Correção na operação de extrato.

--- extrato.py
+++ extrato.py
@@ -4,9 +4,13 @@ text = '''
   #########################################################################################################################                  
   '''
 def extrato(dados):
-  print(text)
-  for dado in dados:
-    for key, value in dado.items():
-      print(f"{key} ---- {value}")
-    print("##########################")
+  if(len(dados) == 0):
+    print(text)
+    print("--------------------   NENHUMA MOVIMENTAÇÃO REALIZADA ------------------------")
+  else: 
+    print(text)
+    for dado in dados:
+      for key, value in dado.items():
+        print(f"{key} ---- {value}")
+      print("##########################")
   return  
