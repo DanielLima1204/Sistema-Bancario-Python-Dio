95e8fa1d9c917bbb8ab5909c7019bfa791a6e87d
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2024-02-28 04:05:40 -0300

    Implementação da operação de Saque utilizada no menu (Main)

--- saque.py
+++ saque.py
@@ -1,22 +1,26 @@
-def saque(*, saldo, saques_realizados):
-  QUANTIDADE_DE_SAQUES_DIARIOS = 3
-  
+from funcoes_uteis.buscando_conta import buscando_conta
+from funcoes_uteis.buscando_usuario import buscando_usuario
+from transacoes.transacao_saque import Saque
+
+
+def saque(lista_usuarios):
   print(" ################## Voçe está na opção de saque. #####################")
-  if(saques_realizados >= QUANTIDADE_DE_SAQUES_DIARIOS):
-    print("Infelizmente vc execedeu o limite de saques diarios!")
-    infor_para_extrato = {'Tipo de Operação:': 'Saque', 'Status': 'Execedeu o número de saques, operação cancelada.'}
-    return saldo, saques_realizados, infor_para_extrato
+  
+  usuario_para_saque = buscando_usuario(lista_usuarios)
+  if usuario_para_saque == None:
+    print("Usuário não encontrado!")
+    return False
+  
+  conta_para_saque = buscando_conta(usuario_para_saque)
+  if conta_para_saque == None:
+    print("Esta conta não existe!")
+    return False
+  
+  valor_do_saque = float(input("Digite o valor para saque: "))
+  saque = Saque(valor_do_saque)
+  
+  if usuario_para_saque.realizar_transacao(conta_para_saque, saque) == True:
+    print(f"Saque Realizado com sucesso. Seu saldo é: {conta_para_saque.saldo:.2f} R$")
+    return True
   else:
-    valor_do_saque = float(input("Digite o valor para saque: "))
-    
-  if(saldo >= valor_do_saque <= 500.00 and valor_do_saque > 0):
-    saldo_anterior = saldo
-    novo_saldo = saldo - valor_do_saque
-    infor_para_extrato = {'Tipo de Operação:': 'Saque', 'Valor de saque: ': f'R${valor_do_saque:.2f}', 'Saldo Anterior': f'R${saldo_anterior:.2f}', 'Saldo Atual': f'R${novo_saldo:.2f}'}
-    
-    print(f"Saque Realizado com sucesso. Seu saldo é: {novo_saldo:.2f} R$")
-    return novo_saldo, saques_realizados + 1, infor_para_extrato
-  else:
-    print("Valor Inválido, Tente novamente.")
-    infor_para_extrato = {'Tipo de Operação:': 'Saque', 'Status': 'Valor inválido, operação cancelada.'}
-    return saldo, saques_realizados, infor_para_extrato 
+    return False
