8e8a2fbce660c87e25d842a7067df7dadf20c098
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2024-02-28 04:03:06 -0300

    Adicionada lógica da Transação de saque.

--- transacoes/transacao_saque.py
+++ transacoes/transacao_saque.py
@@ -4,5 +4,12 @@ class Saque(Transacao):
   def __init__(self, valor):
     self.valor = valor 
   def registrar(self, conta):
-    return conta.sacar(self.valor)
+    if self.valor > conta.saldo:
+      print("Valor para saque maior que o saldo em conta, Operação não realizada.")
+      return False
+    elif conta.saques_realizados >= conta.limite_saques:
+      print("Limite diário de saques ultrapassado, Operação não realizada.")
+      return False
+    else:
+      return conta.sacar(self.valor)
     #TODO Checar o saldo da conta e realizar a operação e retornar um true no caso de sucesso
