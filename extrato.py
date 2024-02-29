e2257636338f406e9775e5412936903333ecf1f1
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2024-02-29 04:20:04 -0300

    Implementação da operação de Extrato.

--- extrato.py
+++ extrato.py
@@ -1,16 +1,34 @@
+from funcoes_uteis.buscando_conta import buscando_conta
+from funcoes_uteis.buscando_usuario import buscando_usuario
+
+
 text = '''
   #########################################################################################################################
                                     DEMONSTRATIVO DE TRANSAÇÕES
   #########################################################################################################################                  
   '''
-def extrato(dados):
-  if(len(dados) == 0):
+def extrato(lista_usuarios):
+  print(" ################## Voçe está na opção de Extrato. #####################")
+  
+  usuario_para_extrato = buscando_usuario(lista_usuarios)
+  if usuario_para_extrato == None:
+    print("Usuário não encontrado!")
+    return False
+  
+  conta_para_extrato = buscando_conta(usuario_para_extrato)
+  if conta_para_extrato == None:
+    print("Esta conta não existe!")
+    return False
+  
+  todas_transacoes = conta_para_extrato.historico.transacoes_realizadas
+  
+  if(len(todas_transacoes) == 0):
     print(text)
     print("--------------------   NENHUMA MOVIMENTAÇÃO REALIZADA ------------------------")
   else: 
     print(text)
-    for dado in dados:
-      for key, value in dado.items():
+    for transacao in todas_transacoes:
+      for key, value in transacao.items():
         print(f"{key} ---- {value}")
       print("##########################")
   return  
