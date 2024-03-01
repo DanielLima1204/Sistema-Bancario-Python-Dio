a81f9c92fbf8d7045165472477beb9c5a0d554d8
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2024-03-01 04:22:22 -0300

    Removido comentários desnecessarios.

--- deposito.py
+++ deposito.py
@@ -5,13 +5,11 @@ from transacoes.transacao_deposito import Deposito
 def deposito(lista_usuarios): 
   print(" ################## Voçe está na opção de déposito. #####################")
   
-  # Encontrando user - Poderia ser feito na main
   usuario_para_deposito = buscando_usuario(lista_usuarios)
   if usuario_para_deposito == None:
     print("Usuário não encontrado!")
     return False 
   
-  # Buscando a conta na instancia do User   
   conta_para_deposito = buscando_conta(usuario_para_deposito)
   if conta_para_deposito == None:
     print("Esta conta não existe!")
@@ -20,7 +18,7 @@ def deposito(lista_usuarios):
   valor_do_deposito = float(input("Digite o valor para déposito: "))
   deposito = Deposito(valor_do_deposito)
   
-  if usuario_para_deposito.realizar_transacao(conta_para_deposito, deposito) == True:
+  if usuario_para_deposito.realizar_transacao(conta_para_deposito, deposito):
     print(f"Déposito Realizado com sucesso. Seu saldo é: {conta_para_deposito.saldo:.2f} R$")
     conta_para_deposito.historico.adicionar_transacao(deposito)
     return True
