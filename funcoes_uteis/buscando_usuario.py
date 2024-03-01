bd24818f01feb8c2fe65efbbf81fd8375bc3493a
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2024-03-01 02:51:30 -0300

    Renomeando variavel pra melhor compreensão.

--- funcoes_uteis/buscando_usuario.py
+++ funcoes_uteis/buscando_usuario.py
@@ -1,8 +1,8 @@
 def buscando_usuario(lista_usuarios):
   cpf_do_titular = input("Digite o cpf do titular da conta: ")
-  user_para_deposito = None
+  user_para_operacao = None
   for user in lista_usuarios:
     if user.cpf == cpf_do_titular:
-      user_para_deposito = user
-  return user_para_deposito
+      user_para_operacao = user
+  return user_para_operacao
       
