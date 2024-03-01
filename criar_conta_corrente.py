442916b4bcf1fd1f83f6555e5249523b3b2d7c1f
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2024-03-01 04:43:50 -0300

    Removendo as classes que foram realocadas.

--- criar_conta_corrente.py
+++ criar_conta_corrente.py
@@ -1,56 +1,6 @@
 from funcoes_uteis.buscando_usuario import buscando_usuario
 from funcoes_uteis.check_conta_corrente import usuario_tem_conta_corrente
-from funcoes_uteis.testes_cpf import testando_cpf_ja_existe
-from Classes.Historico import Historico
-        
-class Conta():
-  def __init__(self, saldo, numero, agencia, cliente):
-    self._saldo = saldo
-    self._numero = numero
-    self._agencia = agencia
-    self._cliente = cliente
-    self.historico = Historico()
-  
-  @property
-  def saldo(self):
-    return self._saldo
-  
-  @property
-  def numero(self):
-    return self._numero  
-  
-  @classmethod
-  def nova_conta(cls):
-    pass
-  
-  def depositar(self, valor):
-    pass
-
-class ContaCorrente(Conta):
-  def __init__(self, limite_saques, saques_realizados, limite_de_saldo, **kw):
-    super().__init__(**kw)
-    self._limite = limite_de_saldo
-    self.saques_realizados = saques_realizados
-    self.limite_saques = limite_saques
-  
-  @classmethod
-  def nova_conta(cls, limite_saques=5, saques_realizados=0, limite_de_saldo=15000.00, **kw):
-    return cls(limite_saques, saques_realizados, limite_de_saldo, **kw)
-  
-  def depositar(self, valor):
-    if valor > 0:
-      self._saldo += valor
-      return True
-    else:
-      return False
-    
-  def sacar(self, valor):
-    if valor > 0:
-      self._saldo -= valor
-      self.saques_realizados += 1
-      return True
-    else:
-      return False
+from Classes.ContaCorrente import ContaCorrente
 
 def criar_conta_corrente(users_list, qtd_contas_correntes):
   text = """
--- criar_usuario.py
+++ criar_usuario.py
@@ -1,42 +1,7 @@
 from funcoes_uteis.menus import exibir_menu_de_confirmacao
 from funcoes_uteis.coletando_dados import coletando_info_usuario      
+from Classes.PessoaFisica import PessoaFisica
 
-class Cliente:
-  def __init__(self, endereco):
-    self._endereco = endereco
-    self._contas = []
-  
-  @property
-  def contas_do_user(self):
-    return self._contas
-  
-  def realizar_transacao(self, conta, transacao):
-    pass
-      
-  def adicionar_conta(self, conta):
-    pass
-
-class PessoaFisica(Cliente):
-  def __init__(self,  cpf, nome, data_de_nascimento, **kw):
-    super().__init__(**kw)
-    self._cpf = cpf
-    self._nome = nome
-    self._data_de_nascimento = data_de_nascimento
-  
-  @property
-  def cpf(self):
-    return self._cpf
-  
-  def realizar_transacao(self, conta, transacao):
-    return transacao.registrar(conta)
-  
-  def adicionar_conta(self, conta):
-  #TODO checar se já existe uma conta corrente com este cpf
-    self._contas.append(conta)
-     
-  def __str__(self):
-    return f"Nome: {self._nome}\tCPF: {self._cpf}\tData de Nascimento: {self._data_de_nascimento}\tContas: {self._contas}"
-  
 def criar_usuario(users_list):
   text = """
 #############################################################################################################################################
